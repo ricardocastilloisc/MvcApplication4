919d0ad9a989c8536e422c2dac56f63749b15bd3
RicardoCastilloISC <ricardocastilloisc@gmail.com>
2019-09-12 15:55:32 -0500

    Initial commit

---
+++ MvcApplication4/Models/Curso_Validar.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace MvcApplication4.Models
+{
+    public class Curso_Validar
+    {
+        [DisplayName("Código del curso")]
+        public int codigo { get; set; }
+
+        [DisplayName("Nombre del Curso")]
+        [Required(ErrorMessage="Nombre del curso es reuerido")]
+        [StringLength(30, MinimumLength = 3, ErrorMessage = "No más de 30 caracteres")]
+        public string nombre { get; set; }
+
+        [DisplayName("Email del Curso")]
+        [Required(ErrorMessage =  "Email del curso requerido")]
+        [StringLength(50, ErrorMessage = "No más de 50 caracteres")]
+        [EmailAddress(ErrorMessage="Email invalido")]
+        public string correo { get; set; }
+
+        [DisplayName("Crédito del curso")]
+        [Required(ErrorMessage="Número de crédito de curso es requrido")]
+        [Range(1,6)]
+        public int credito { get; set; }
+    }
+}
---
+++ MvcApplication4/Models/ValidaModel.Designer.cs
@@ -0,0 +1,4 @@
+﻿// La generación de código predeterminada está deshabilitada para el modelo 'C:\Users\ricar\OneDrive\Documentos\Visual Studio 2012\Projects\MvcApplication4\MvcApplication4\Models\ValidaModel.edmx'. 
+// Para habilitar la generación de código predeterminada, cambie el valor de la propiedad del diseñador 'Estrategia de generación de código'
+// por otro valor. Esta propiedad está disponible en la ventana Propiedades cuando se abre
+// el modelo en el diseñador.
